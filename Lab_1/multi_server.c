fb9565ef423b71b1b7ea19b88ff369464a51dcd5
David Jacob Tinley <djt@Davids-Air.attlocal.net>
2023-09-29 08:10:57 -0400

    Adding multi thread server in C

---
+++ Lab_1/multi_server.c
@@ -0,0 +1,15 @@
+//
+//
+//
+//
+//
+//
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT = 8080;
+#define HOST_IP = "0.0.0.0";
