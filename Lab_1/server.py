af29daf24b45809e8d55e58f199fe67fdbefbf8e
David-J-Tinley <dtinley@kent.edu>
2023-11-06 20:33:05 -0500

    Lab 1 Complete

--- Lab_1/server.py
+++ Lab_1/server.py
@@ -1,67 +1,54 @@
 #!/usr/bin/python3
 
-####################################
+###############################################################################
 # server.py
 #
 # Lab 1
 # David J Tinley
 # 09/28/2023
 #
-# Objective - Develop a web server
-# that handles one HTTP request at
-# a time.
-####################################
+# Objective - Develop a web server that handles one HTTP request at a time.
+###############################################################################
 
 import socket
-
-# import sys
 
 PORT = 8080  # use port 8080
 HOST = "0.0.0.0"  # listen for all address's
 
+print(f"Server listening on http://{HOST}:{PORT}")
 
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(1)
-    client_socket, client_address = server_socket.accept()
-    print(f"Server listening on http://{HOST}:{PORT}")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
 
-    while True:
-        print("Waiting...")
+while True:
+    client_socket = None  # Used to suppress Pylance linting error
+    print("Waiting...")
+    try:
+        client_socket, client_address = server_socket.accept()
+        print(f"Request received from {client_address}")
 
-        try:
-            print(f"Request received from {client_address}")
+        file_request = client_socket.recv(1024).decode("utf-8")
+        file_name = file_request.split()[1]
+        file = open(file_name[1:])
 
-            file_request = client_socket.recv(1024).decode("utf-8")
+        output_data = file.read()
+        header_response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
+        client_socket.sendall(header_response.encode("utf-8"))
 
-            file_name = file_request.split()[1]
+        for i in range(0, len(output_data)):
+            client_socket.send(output_data[i].encode())
 
-            file = open(file_name[1:])
+        client_socket.send("\r\n".encode())
+        client_socket.close()
 
-            output_data = file.read()
-
-            header_response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
-
-            client_socket.sendall(header_response.encode("utf-8"))
-
-            for i in range(0, len(output_data)):
-                client_socket.send(output_data[i].encode())
-
-            client_socket.send("\r\n".encode())
-
+    except FileNotFoundError:
+        if client_socket is not None:  # Used to suppress Pylance linting error
+            client_socket.sendall(
+                b"HTTP/1.1 404 Not Found\nContent-Type: text/html\n\nFile Not Found"
+            )
             client_socket.close()
 
-        except IOError:
-            client_socket.sendall(b"Error: File Not Found...")
-
-        finally:
+    finally:
+        if client_socket is not None:  # Used to suppress Pylance linting error
             client_socket.close()
-
-    # unreachable?
-    # server_socket.close()
-    # sys.exit()
-
-
-if __name__ == "__main__":
-    main()
